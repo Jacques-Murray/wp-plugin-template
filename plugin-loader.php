7ad116caad269155dacdda0b77e8f56a4234e392
Jacques Murray <jacquesmmurray@gmail.com>
2025-12-04 16:38:07 +0200

    feat: Add initial placeholder plugin loader file with basic WordPress headers.

---
+++ plugin-loader.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Local Dev Plugin
+ * Description: This is a placeholder. Rename this file and update headers for your actual project.
+ * Version: 1.0.0
+ * Author: Your Name
+ */
+
+// Your code or include/require statements go here
